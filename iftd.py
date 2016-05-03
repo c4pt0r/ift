e67dc3ed9d57017fad36bbbb707f636034905766
dongxu <huang@wandoujia.com>
2016-05-04 00:40:53 +0800

    backup

--- iftd.py
+++ iftd.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
-
 import os, sys
 import time
+import toml
+
 from daemon import Daemon
+from distutils.dir_util import mkpath
 
-class IftDaemon(Daemon):
-    def run(self):
-        while True:
-            # TODO
-            time.sleep(1)
+RESOURCE_PATH = '/var/ift'
 
-if __name__ == '__main__':
-    daemon = IftDaemon('/tmp/ift.pid')
+def load_profiles():
+    profiles_dir = os.path.join(RESOURCE_PATH, 'profiles')
+    mkpath(profiles_dir)
+    profiles = []
+    for fn in os.listdir(profiles_dir):
+        if not profile_fn.endswith('.toml'):
+            continue
+        profile_fn = os.path.join(profiles_dir, fn)
+        with open(profile_fn) as fp:
+            config = toml.loads(fp.read())
+            print profile_fn, config
+            profiles.append(config)
+    return profiles
+
+def main_loop():
+    load_profiles()
+    while True:
+        time.sleep(1)
+
+def daemon():
+    class _daemon(Daemon):
+        def run(self): main_loop()
+
+    daemon = _daemon('/tmp/ift.pid')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()
@@ -26,3 +46,7 @@ if __name__ == '__main__':
     else:
         print "usage: %s start|stop|restart" % sys.argv[0]
         sys.exit(2)
+    
+if __name__ == '__main__':
+    #run_as_daemon()
+    main_loop()
